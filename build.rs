f34997252506a0aeb8a5d77de2c283d2c389420c
Sean Gillespie <sean@swgillespie.me>
2026-06-05 12:23:47 -0700

    fix CI and add licenses

--- build.rs
+++ build.rs
@@ -1,6 +1,8 @@
-use std::io::Result;
-
-fn main() -> Result<()> {
-    prost_build::compile_protos(&["proto/krpc.proto"], &["proto/"])?;
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // protox compiles the schema in pure Rust, so neither builders nor
+    // docs.rs need a system `protoc`.
+    println!("cargo:rerun-if-changed=proto/krpc.proto");
+    let file_descriptors = protox::compile(["proto/krpc.proto"], ["proto/"])?;
+    prost_build::Config::new().compile_fds(file_descriptors)?;
     Ok(())
 }
